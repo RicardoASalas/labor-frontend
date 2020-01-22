5edc543e47f978880c09b83e19a6bfb4fbbbbc92
Icaruk <adri-rk@hotmail.com>
2020-01-22 23:12:06 +0100

    Antd uninstalled

--- src/App.js
+++ src/App.js
@@ -2,18 +2,17 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
+// CSS
+import "./global.css";
+import "./App.css";
+
 // Componentes
 import Home from "./containers/home/home";
 import Header from "./components/header/header";
 import Login from "./containers/login/login";
 import Profile from "./containers/profile/profile";
 import Register from "./containers/register/register";
-
-
-// CSS
-import "./global.css";
-import "antd/dist/antd.css";
-import "./App.css";
+import Search from "./containers/search/search";
 
 
 export default function App() {
@@ -27,6 +26,7 @@ export default function App() {
 					<Route path="/" exact component={Home} />
 					<Route path="/login" exact component={Login} />
 					<Route path="/register" exact component={Register} />
+					<Route path="/search" exact component={Search} />
 					<Route path="/profile" exact component={Profile} />
 					
 				</Switch>
