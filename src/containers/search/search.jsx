9898130b4de58a9e0b3273d2536efecb579b9a96
Icaruk <adri-rk@hotmail.com>
2020-01-23 14:02:00 +0100

    Vista resultado de búsqueda terminada + componente searchResult

--- src/containers/search/search.jsx
+++ src/containers/search/search.jsx
@@ -5,7 +5,9 @@ import "./search.scss";
 
 import { FormControl, Radio, TextField, FormControlLabel } from '@material-ui/core';
 import TextInput from "../../components/textInput/textInput";
-import ImageLabor from "../../components/image/image";
+// import ImageLabor from "../../components/image/image";
+import SearchResultLabor from "../../components/searchResult/searchResult";
+
 
 export default class Search extends React.Component {
 	
@@ -113,45 +115,46 @@ export default class Search extends React.Component {
 					
 					<div className="results br">
 						
-						<div className="resultCard pt3 flex-dir-r">
-							
-							<div className="col1 flex-dir-c">
-								
-								<div className="image">
-									<ImageLabor
-										className="br"
-										src="https://about.canva.com/wp-content/uploads/sites/3/2016/08/logos-1.png"
-										w={100}
-										alt="imagen de la empresa"
-										measure="px"
-										// border radius
-									/>
-								</div>
-							</div>
-							
-							<div className="col2 flex-dir-c">
-								<h2 className="title">Full stack developer (MERN stack)</h2>
-								<h2 className="companyName">TechnoCode3000 Inc.</h2>
-								
-								<div className="row1 flex-dir-r">
-									<p>Province | </p>
-									<p>| Date</p>
-								</div>
-								
-								<p>
-									Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint repellat aut dolore voluptatibus magni impedit animi? Assumenda sit nesciunt et, modi, delectus facilis unde dicta quidem incidunt nam accusantium odio?
-									Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati ullam, ut natus, vel facere animi perferendis sapiente quaerat earum voluptatum, minima optio fugit! Fugit, repudiandae quo nobis quidem porro dolores?
-								</p>
-								
-								<div className="row2 flex-dir-r">
-									<p>Contract type |</p>
-									<p>Hours / week |</p>
-									<p>Salary</p>
-								</div>
-								
-							</div>
-							
-						</div>
+						<SearchResultLabor
+							className="mb3"
+							img={"https://about.canva.com/wp-content/uploads/sites/3/2016/08/logos-1.png"}
+							title="Full stack developer (MERN stack)"
+							companyName="Nombre de empresa 02"
+							description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint repellat aut dolore voluptatibus magni impedit animi? Assumenda sit nesciunt et, modi, delectus facilis unde dicta quidem incidunt nam accusantium odio?"
+							city="Valencia"
+							date="21 ene"
+							contractType="Fulltime"
+							minHoursWeek={20}
+							maxHoursWeek={25}
+							minSalary={15000}
+							maxSalary={16000}
+						/>
+						<SearchResultLabor
+							img={"https://about.canva.com/wp-content/uploads/sites/3/2016/08/logos-1.png"}
+							title="Full stack developer (MERN stack)"
+							companyName="Nombre de empresa 02"
+							description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint repellat aut dolore voluptatibus magni impedit animi? Assumenda sit nesciunt et, modi, delectus facilis unde dicta quidem incidunt nam accusantium odio?"
+							city="Valencia"
+							date="21 ene"
+							contractType="Fulltime"
+							minHoursWeek={20}
+							maxHoursWeek={25}
+							minSalary={15000}
+							maxSalary={16000}
+						/>
+						<SearchResultLabor
+							img={"https://about.canva.com/wp-content/uploads/sites/3/2016/08/logos-1.png"}
+							title="Full stack developer (MERN stack)"
+							companyName="Nombre de empresa 02"
+							description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint repellat aut dolore voluptatibus magni impedit animi? Assumenda sit nesciunt et, modi, delectus facilis unde dicta quidem incidunt nam accusantium odio?"
+							city="Valencia"
+							date="21 ene"
+							contractType="Fulltime"
+							minHoursWeek={20}
+							maxHoursWeek={25}
+							minSalary={15000}
+							maxSalary={16000}
+						/>
 						
 					</div>
 					
