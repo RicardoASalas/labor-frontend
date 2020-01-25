ec0f1990654297898cb5dd086e9c176fd2ade119
Icaruk <adri-rk@hotmail.com>
2020-01-25 17:37:27 +0100

    Eliminado value={} en el componente textInput, para que haya una única fuente de la verdad

--- src/components/textInput/textInput.jsx
+++ src/components/textInput/textInput.jsx
@@ -72,7 +72,7 @@ export default class TextInput extends React.Component {
 					variant="outlined"
 					// onChange={ (ev) => this.inputToStatus(ev, stateKey) }
 					onChange={ this.props.onChange }
-					value={ this.props.value }
+					// value={ this.props.value }
 				/>
 			</FormControl>
 			
