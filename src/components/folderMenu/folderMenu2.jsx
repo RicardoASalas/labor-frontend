68b77f084c86cea3bd0129bd392bfa8a567b829b
Icaruk <adri-rk@hotmail.com>
2020-02-07 12:40:07 +0100

    Fix inyección de CSS desde folderMenu hacia fuera

--- src/components/folderMenu/folderMenu2.jsx
+++ src/components/folderMenu/folderMenu2.jsx
@@ -50,28 +50,28 @@ class FolderMenu extends React.Component {
         
 
         let candidatesArray = this.state.filteredCandidates.map(candidate=>
-                            <span className="results">
+			<span className="results">
 
-                                <p className="fields">{candidate.name?candidate.name:""}</p>
-                                <p className="fields">{candidate.surname?candidate.surname:""}</p>
-                                <p className="fields">{candidate.email?candidate.email:""}</p>
-                                <p className="fields">{candidate.nif?candidate.nif:""}</p>
-                                <p className="fields">{candidate._offerTitle?candidate._offerTitle:""}</p>
-                                <StatusTranslator status={candidate._status}/>
+				<p className="fields">{candidate.name?candidate.name:""}</p>
+				<p className="fields">{candidate.surname?candidate.surname:""}</p>
+				<p className="fields">{candidate.email?candidate.email:""}</p>
+				<p className="fields">{candidate.nif?candidate.nif:""}</p>
+				<p className="fields">{candidate._offerTitle?candidate._offerTitle:""}</p>
+				<StatusTranslator status={candidate._status}/>
 
-                            </span>)
+			</span>)
 
         return (
             <div className="companyMenu">
-            <div className="companyMenuLinks">
-                <p className="menuLink" onClick={()=>this.getCandidateInfo(0)}>Pendiente</p>
-                <p className="menuLink" onClick={()=>this.getCandidateInfo(1)}>Revision</p>
-                <p className="menuLink" onClick={()=>this.getCandidateInfo(2)}>Rechazado</p>
-            </div>
-            <div className="companyMenuResults">
-                {candidatesArray}
-            </div>
-        </div>        
+				<div className="companyMenuLinks">
+					<p className="menuLink" onClick={()=>this.getCandidateInfo(0)}>Pendiente</p>
+					<p className="menuLink" onClick={()=>this.getCandidateInfo(1)}>Revision</p>
+					<p className="menuLink" onClick={()=>this.getCandidateInfo(2)}>Rechazado</p>
+				</div>
+				<div className="companyMenuResults">
+					{candidatesArray}
+				</div>
+			</div>        
         )
      }
     
